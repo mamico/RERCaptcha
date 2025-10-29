3a0b92228ae718b3335057f99052692f40a09312
Mauro Amico <mauro.amico@gmail.com>
2025-10-30 00:09:00 +0100

    bootstrap

--- demo001/app.py
+++ demo001/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template_string, request
+from flask import Flask, render_template, request
 import requests
 import os
 import time
@@ -12,36 +12,17 @@ CAPJS_PUBLIC_URL = os.environ.get("CAPJS_PUBLIC_URL", "http://localhost:3000")
 CAPJS_SITE_KEY = os.environ.get("SITE_KEY")
 CAPJS_SECRET = os.environ.get("SECRET_KEY")
 
-FORM_TEMPLATE = """
-<!doctype html>
-<html>
-  <body>  
-    <h1>Form con CapJS</h1>
-    <p>{{ message }}</p>
-    <form action="/" method="post">
-      <cap-widget 
-        id="cap" 
-        data-cap-api-endpoint="{{ capjs_public_url }}/{{site_key}}/"
-        data-cap-hidden-field-name="capjs-token"></cap-widget>
-      <input type="text" name="username" placeholder="Username"><br><br>
-      <br>
-      <button type="submit">Invia</button>
-    </form>
-    <script>
-      window.CAP_CUSTOM_WASM_URL = "{{ capjs_public_url }}/assets/cap_wasm.js";
-    </script>
-    <script src="{{ capjs_public_url }}/assets/widget.js"></script>
-  </body>
-</html>
-"""
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route("/", methods=["GET", "POST"])
-def testvisibleok():
+@app.route("/visible-it", methods=["GET", "POST"])
+def visible_it():
     message = ""
     status_code = 200
     if request.method == "POST":
         token = request.form.get("capjs-token")
-        username = request.form.get("username")
+        text = request.form.get("text")
 
         if not token:
             # return "Errore: token mancante", 400
@@ -56,7 +37,7 @@ def testvisibleok():
         if res:
             result = res.json()
             if result.get("success"):
-                message = f"Captcha OK ✅, utente: {username}, token: {token}, captcha_result: {json.dumps(result)}"
+                message = f"Captcha OK ✅, campo di testo: {text}, token: {token}, captcha_result: {json.dumps(result)}"
                 status_code = 200
             else:
                 message = f"Captcha NON valido ❌: {result}, token: {token}, captcha_result: {json.dumps(result)}"
@@ -65,12 +46,51 @@ def testvisibleok():
             message = f"Errore non previsto nella verifica del token: {token} status: {res.status_code} text: {res.text}"
             status_code = res.status_code
 
-    return render_template_string(
-      FORM_TEMPLATE, 
+    return render_template(
+      "visible-it.html", 
       capjs_public_url=CAPJS_PUBLIC_URL, 
       site_key=CAPJS_SITE_KEY,
       message=message,
     ), status_code
+
+
+@app.route("/invisible", methods=["GET", "POST"])
+def invisible():
+    message = ""
+    status_code = 200
+    if request.method == "POST":
+        token = request.form.get("capjs-token")
+        text = request.form.get("text")
+
+        if not token:
+            # return "Errore: token mancante", 400
+            message = "Errore: token mancante"
+            status_code = 400
+
+        res = requests.post(
+            f"{CAPJS_INTERNAL_URL}/{CAPJS_SITE_KEY}/siteverify",
+            data={"secret": CAPJS_SECRET, "response": token},
+            timeout=5
+        )
+        if res:
+            result = res.json()
+            if result.get("success"):
+                message = f"Captcha OK ✅, campo di testo: {text}, token: {token}, captcha_result: {json.dumps(result)}"
+                status_code = 200
+            else:
+                message = f"Captcha NON valido ❌: {result}, token: {token}, captcha_result: {json.dumps(result)}"
+                status_code = 400
+        else:
+            message = f"Errore non previsto nella verifica del token: {token} status: {res.status_code} text: {res.text}"
+            status_code = res.status_code
+
+    return render_template(
+      "invisible.html",
+      capjs_public_url=CAPJS_PUBLIC_URL,
+      site_key=CAPJS_SITE_KEY,
+      message=message,
+    ), status_code
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
